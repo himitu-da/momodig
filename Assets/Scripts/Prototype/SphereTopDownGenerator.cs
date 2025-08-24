bd6639c3a982fca6a14afbe8bd5766ef68a8487f
himitu-da <0127himitu@gmail.com>
2025-08-25 01:32:03 +0900

    ファイル構造の変更、プロトタイプの未使用の物を削除

--- Assets/Scripts/Prototype/SphereTopDownGenerator.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class SphereTopDownGenerator : BasePlayerGenerator
-{
-    protected override PlayerController.MoveMode moveMode => PlayerController.MoveMode.TopDown;
-}
