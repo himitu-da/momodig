4e0e04f19513c460f20b32c1866f617e73b0d789
Busunekoya <92299420+Busunekoya@users.noreply.github.com>
2025-09-22 10:51:32 +0900

    タイトルシーンから「開始」「終了」を設定しました
    設定は未実装です

--- Assets/Scripts/Title/Title_Button_System.cs
+++ Assets/Scripts/Title/Title_Button_System.cs
@@ -12,6 +12,7 @@ public class Title_Button_System : MonoBehaviour
     public Sprite SettingSpriteChoice;
     public Sprite FinishSprite;
     public Sprite FinishSpriteChoice;
+    public ChangeScene changescene;
     private int keyplace = 0;
     public void OnMove(InputAction.CallbackContext value)       //タイトルにおけるキー取得、現在上下矢印キーのみ認識
     {
@@ -44,5 +45,30 @@ public class Title_Button_System : MonoBehaviour
         {
             button.sprite = inactivebutton;
         }
+    }
+    public void Onselect(InputAction.CallbackContext value)
+    {
+        if(value.performed){
+            SelectAction(keyplace);
+        }
+    }
+    void SelectAction(int SelectKey){
+        //開始ボタンが押されている
+        if(SelectKey % 3 == 0){
+            changescene.OnClickToChangeScene("OverWorldScene");
+        }
+        //設定ボタンが押されている
+        if(keyplace % 3 == 1 || keyplace % 3 == -2){
+
+        }
+        //終了ボタンが押されている
+        if(keyplace % 3 == 2 || keyplace % 3 == -1){
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
     }
 }
