7aea7bacb1b4985d9165b8211b87173435b90dd6
Busunekoya <92299420+Busunekoya@users.noreply.github.com>
2025-10-03 14:00:02 +0900

    要求素材表示用ゲームオブジェクトを登録しました

--- Assets/Scripts/ShopScene/ContentManager.cs
+++ Assets/Scripts/ShopScene/ContentManager.cs
@@ -11,6 +11,19 @@ public class ContentManager : MonoBehaviour
     [SerializeField] private ItemData[] items;
     [SerializeField] private TMPro.TMP_Text productname;
     [SerializeField] private TMPro.TMP_Text flavortext;
+    [SerializeField] private GameObject Stone_Request;
+    [SerializeField] private GameObject DragonOre_Request;
+    [SerializeField] private GameObject Iron_Request;
+    [SerializeField] private GameObject Tin_Request;
+    [SerializeField] private GameObject Nickel_Request;
+    [SerializeField] private GameObject Sillicon_Request;
+    [SerializeField] private GameObject Cobalt_Request;
+    [SerializeField] private GameObject Titanium_Request;
+    [SerializeField] private GameObject Sulfur_Request;
+    [SerializeField] private GameObject Tungsten_Request;
+    [SerializeField] private GameObject Hihiirokane_Request;
+    [SerializeField] private GameObject Gold_Request;
+    [SerializeField] private GameObject Rareearth_Request;
     void Start()
     {
         foreach (ItemData item in items)
@@ -18,7 +31,7 @@ public class ContentManager : MonoBehaviour
             GameObject setitem = Instantiate(itemUIPrefab, scrollview);
             ItemUI ui = setitem.GetComponent<ItemUI>();
             Toggle toggleset = setitem.GetComponent<Toggle>();
-            ui.SetItem(item,productname,flavortext);
+            ui.SetItem(item,productname,flavortext,Stone_Request,DragonOre_Request,Iron_Request,Tin_Request,Nickel_Request,Sillicon_Request,Cobalt_Request,Titanium_Request,Sulfur_Request,Tungsten_Request,Hihiirokane_Request,Gold_Request,Rareearth_Request);
             ui.item_name = item.Itemname;
             ui.FlavorText = item.FlavorText;
             toggleset.group = group;
--- Assets/Scripts/ShopScene/ItemUI.cs
+++ Assets/Scripts/ShopScene/ItemUI.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 public class ItemUI : MonoBehaviour
 {
     private TMPro.TMP_Text productname;
@@ -8,11 +9,39 @@ public class ItemUI : MonoBehaviour
     public string item_name;
     [TextArea] public string FlavorText;
     [SerializeField] private Text nametext;
-    public void SetItem(ItemData item, TMPro.TMP_Text product, TMPro.TMP_Text content)
+    private GameObject Stone_Request;
+    private GameObject DragonOre_Request;
+    private GameObject Iron_Request;
+    private GameObject Tin_Request;
+    private GameObject Nickel_Request;
+    private GameObject Sillicon_Request;
+    private GameObject Cobalt_Request;
+    private GameObject Titanium_Request;
+    private GameObject Sulfur_Request;
+    private GameObject Tungsten_Request;
+    private GameObject Hihiirokane_Request;
+    private GameObject Gold_Request;
+    private GameObject Rareearth_Request;
+    private List<RequestMaterial> materiallist;
+    public void SetItem(ItemData item, TMPro.TMP_Text product, TMPro.TMP_Text content, GameObject Stone_Request, GameObject DragonOre_Request, GameObject Iron_Request, GameObject Tin_Request, GameObject Nickel_Request, GameObject Sillicon_Request, GameObject Cobalt_Request, GameObject Titanium_Request, GameObject Sulfur_Request, GameObject Tungsten_Request, GameObject Hihiirokane_Request, GameObject Gold_Request, GameObject Rareearth_Request)
     {
         nametext.text = item.Itemname;
         productname = product;
         contentdescription = content;
+        materiallist = item.requestmaterials;
+        this.Stone_Request = Stone_Request;
+        this.DragonOre_Request = DragonOre_Request;
+        this.Iron_Request = Iron_Request;
+        this.Tin_Request = Tin_Request;
+        this.Nickel_Request = Nickel_Request;
+        this.Sillicon_Request = Sillicon_Request;
+        this.Cobalt_Request = Cobalt_Request;
+        this.Titanium_Request = Titanium_Request;
+        this.Sulfur_Request = Sulfur_Request;
+        this.Tungsten_Request = Tungsten_Request;
+        this.Hihiirokane_Request = Hihiirokane_Request;
+        this.Gold_Request = Gold_Request;
+        this.Rareearth_Request = Rareearth_Request;
     }
     public void ChangeInfo()
     {
