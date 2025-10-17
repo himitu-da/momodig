bee0d80babbc8bdfa8a029e3cc93ad0fb6883b14
Busunekoya <92299420+Busunekoya@users.noreply.github.com>
2025-10-17 15:08:47 +0900

    色々微修正

--- Assets/Scripts/ShopScene/ItemDataManager.cs
+++ Assets/Scripts/ShopScene/ItemDataManager.cs
@@ -13,22 +13,66 @@ public class ItemData : ScriptableObject
     public List<RequestMaterial> requestmaterials = new List<RequestMaterial>();
     [Header("強化レベル")]
     public int itemlevel = 0;
+    [Header("レベル補正種")]
+    public ItemUpdateType updatetype = ItemUpdateType.factorial;
 }
 [System.Serializable]
 public class RequestMaterial
 {
     public ResourceType type;       //Common/ResourceTypesを見る
     public int amount;
-    public int correctamount(int level)
+    public int correctamount(ItemUpdateType type,int level)
     {
-        return amount * factorialnum(level);
+        return amount * updatetypenum(type,level);
+    }
+    int updatetypenum(ItemUpdateType type, int level)
+    {
+        int returnnum = 1;
+        if (type == ItemUpdateType.factorial)
+        {
+            returnnum = factorialnum(level);
+        }
+        else if (type == ItemUpdateType.nochange)
+        {
+            returnnum = 1;
+        }
+        else if (type == ItemUpdateType.multiply)
+        {
+            returnnum = level;
+        }else if(type == ItemUpdateType.fibonacci)
+        {
+            returnnum = fibonacci(level);
+        }
+        return returnnum;
+    }
+    int fibonacci(int level)
+    {
+        int current = 1;
+        int next = 1;
+        int tmp;
+
+        for (int i = 1; i < level; i++)
+        {
+            tmp = current + next;
+            current = next;
+            next = tmp;
+        }
+        return current;
     }
     int factorialnum(int level)
     {
         int factorialresult = 1;
-        for(int i = 0; i < level; i++){
-            factorialresult = factorialresult * (i+1);
+        for (int i = 0; i < level; i++)
+        {
+            factorialresult = factorialresult * (i + 1);
         }
         return factorialresult;
     }
+}
+public enum ItemUpdateType
+{
+    factorial,
+    nochange,
+    multiply,
+    fibonacci
 }
